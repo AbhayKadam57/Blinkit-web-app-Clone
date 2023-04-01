588025434df29ff4fce4724a5dc80b7224451a6e
abhay <kadamabhay40@gmail.com>
2023-04-01 16:09:44 +0530

    deploy url added

--- client/src/apiRequest.js
+++ client/src/apiRequest.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import jwt_decode from "jwt-decode";
 
 export const publicRequest = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://starlit-pavlova-4ad9d8.netlify.app",
 });
 
 export const userRequest = axios.create({
-  baseURL: "http://localhost:5500/api",
+  baseURL: "https://starlit-pavlova-4ad9d8.netlify.app",
 });
 
 const NewRefershTokenGenrate = async () => {
