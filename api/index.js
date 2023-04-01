53d6b30b4b232d263040dff270217d8f976aefae
abhay <kadamabhay40@gmail.com>
2023-04-01 17:43:51 +0530

    new last commit

--- api/index.js
+++ api/index.js
@@ -40,20 +40,6 @@ app.use("/api/cart", CartRoutes);
 app.use("/api/order", OrderRoutes);
 app.use("/api/payment", PaymentRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "client", "build", "index.html"),
-      function (err) {
-        if (err) {
-          res.status(500).send(err);
-        }
-      }
-    );
-  });
-}
 app.listen(PORT, () => {
   Connect();
   console.log(`Server is started ${PORT}`);
