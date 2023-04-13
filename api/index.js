ee136528d87dba212905c8e8ce562e9720aafcb4
abhay <kadamabhay40@gmail.com>
2023-04-14 01:42:47 +0530

    api/index.js updated

--- api/index.js
+++ api/index.js
@@ -43,12 +43,12 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
 
-app.use("/api/user", UserAuthRoutes);
-app.use("/api/userOperation", UserOperationRoutes);
-app.use("/api/product", ProductRoutes);
-app.use("/api/cart", CartRoutes);
-app.use("/api/order", OrderRoutes);
-app.use("/api/payment", PaymentRoutes);
+app.use("api/user", UserAuthRoutes);
+app.use("api/userOperation", UserOperationRoutes);
+app.use("api/product", ProductRoutes);
+app.use("api/cart", CartRoutes);
+app.use("api/order", OrderRoutes);
+app.use("api/payment", PaymentRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
